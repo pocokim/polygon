f09c1b83dd4ea050b5334019263b9c29c57f2917
zwonH <a.circle.one@gmail.com>
2020-03-22 17:46:44 +0900

    함수 정리

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
 const f = require('./Calculator');
 
-f.getArea('circle', 10, 10);
+console.log('원의 넓이 ' + f.getArea('circle', 10));
+console.log('원의 넓이의 합 ' + f.getArea('circle', 10, 10));
+console.log('사각형의 넓이: ' + f.getArea('rect', 10, 15));
+console.log('사다리꼴의 넓이: ' + f.getArea('trapezoid', 10, 15, 10));
+
+console.log('원의 넓이 ' + f.getCircle(10));
+console.log('사각형의 넓이: ' + f.getRectangle(10, 15));
+console.log('사다리꼴의 넓이: ' + f.getTrapezoid(10, 15, 10));
--- Calculator.js
+++ Calculator.js
@@ -1,46 +1,47 @@
 //원의 1개의 넓이 구하는 함수
-const circleArea = r => {
+const getCircle = r => {
   let circleArea = r * r * Math.PI;
   return circleArea;
 };
 //원의 넓이의 합을 구하는 재귀 함수
-const sumCircleAreas = (r, n) => {
+const sumCircle = (r, n) => {
   if (n === 0) {
     return 0;
   } else {
-    return circleArea(r) + sumCircleAreas(r + 1, n - 1);
+    return getCircle(r) + sumCircle(r + 1, n - 1);
   }
 };
 //사각형의 넓이 구하는 함수 (가로, 세로)
-const calRectangleArea = (a, b) => {
-  let rectangleArea = a * b;
-  console.log('사각형의 넓이: ' + rectangleArea);
+const getRectangle = (w, h) => {
+  let rectangleArea = w * h;
+  return rectangleArea;
 };
 //사다리꼴의 넓이 구하는 함수 (윗변, 아랫변, 높이)
-const calTrapezoidArea = (a, b, h) => {
-  let trapezoidArea = (a + b) * h / 2;
-  console.log('사다리꼴의 넓이: ' + trapezoidArea);
+const getTrapezoid = (uw, dw, h) => {
+  let trapezoidArea = (uw + dw) * h / 2;
+  return trapezoidArea;
 };
 
 //입력된 인자에 따라 알맞은 함수 호출하여 넓이 계산
-const getArea = (polygon, a, b, c) => {
+const getArea = (polygon, ...params) => {
+  let area = 0;
   switch (polygon) {
     case 'circle':
-      if (b === undefined) {
-        console.log('원의 넓이: ' + circleArea(a));
-      } else {
-        console.log('원의 넓이의 합: ' + sumCircleAreas(a, b));
-      }
+      area = params.length === 2 ? sumCircle(...params) : getCircle(params[0]);
       break;
     case 'rect':
-      calRectangleArea(a, b);
+      area = getRectangle(...params);
       break;
     case 'trapezoid':
-      calTrapezoidArea(a, b, c);
+      area = getTrapezoid(...params);
       break;
   }
+  return area;
 };
 
 module.exports = {
-  getArea
+  getArea,
+  getCircle,
+  getRectangle,
+  getTrapezoid
 };
